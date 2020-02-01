b728ead7e486e3ff424f9b06f1a58b89e91f744e
Giacomo Pignoni <giacomo.pignoni@gmail.com>
2020-02-01 13:53:11 +0100

    Added logic for idol

--- Assets/Scripts/Idol.cs
+++ Assets/Scripts/Idol.cs
@@ -6,10 +6,42 @@ public class Idol : MonoBehaviour
     [Range(1, 2)]
     public int level;
     public int point;
-    public bool repaired;
+    public IdolRepairedStatus status;
 
     public void ResetStats()
     {
-
+        status = IdolRepairedStatus.broken;
     }
+
+    public void StatusUp()
+    {
+        switch(level)
+        {
+            case 1:
+                status = IdolRepairedStatus.repaired;
+                break;
+            case 2:
+                if(status == IdolRepairedStatus.semiBorken)
+                {
+                    status = IdolRepairedStatus.repaired;
+                }
+                else
+                {
+                    status = IdolRepairedStatus.semiBorken;
+                }
+                break;
+        }
+    }
+
+    public void Broke()
+    {
+        status = IdolRepairedStatus.broken;
+    }
+}
+
+public enum IdolRepairedStatus
+{
+    broken,
+    semiBorken,
+    repaired
 }
