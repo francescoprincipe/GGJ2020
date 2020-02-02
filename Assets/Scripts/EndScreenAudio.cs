c74f6154fb5244ae5e2fd9a31e72a8466bcba293
francescoprincipe <fra97.p@gmail.com>
2020-02-02 11:28:56 +0100

    Added soundtrack

---
+++ Assets/Scripts/EndScreenAudio.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndScreenAudio : MonoBehaviour
+{
+    public AudioClip intro;
+    public AudioClip loop;
+    private AudioSource audsource;
+
+    private void Awake()
+    {
+        audsource = GetComponent<AudioSource>();
+    }
+    void Start()
+    {
+        StartCoroutine(playSoundinSequence());
+    }
+    private IEnumerator playSoundinSequence()
+    {
+
+        audsource.clip = intro;
+        audsource.Play();
+        yield return new WaitForSeconds(audsource.clip.length);
+        audsource.loop = true;
+        audsource.clip = loop;
+        audsource.Play();
+    }
+    
+}
